20695da59e0e990ea993673fe653bf8305a1f93e
carlosesperillamesias <carlos.esperilla@gmail.com>
2025-10-30 19:15:29 +0100

    Modificar titulo del tab del navegador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { MenuComponent } from './menu/menu.component';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -17,9 +19,13 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'trazardo-frontend';
-  constructor(private router: Router) {}
+  constructor(private router: Router, private titleService: Title) {}
 
     get isLoginPage(): boolean {
       return this.router.url === '/login';
     }
+
+  ngOnInit(): void {
+      this.titleService.setTitle('Dehesa de Extremadura'); // Establece el título global
+  }
 }
